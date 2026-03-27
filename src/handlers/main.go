60a80ee481bd767d9611bc3712d216b69f7c0812
SanketJawali <135835658+SanketJawali@users.noreply.github.com>
2026-03-27 11:24:08 +0530

    Added caching for target server fetching. Cleaned up some comments

--- src/handlers/main.go
+++ src/handlers/main.go
@@ -20,7 +20,7 @@ type HandlerFunc struct {
 }
 
 func (handler HandlerFunc) RequestHandler(w http.ResponseWriter, r *http.Request) {
-	// 1. Set an temperory variable to
+	// 1. Extracting the Target ID, and route from the URL path
 	// The URL path is expected to be in the format /{id}/... where {id} is the target ID
 	if len(r.URL.Path) < 2 {
 		http.Error(w, "No ID provided", http.StatusBadRequest)
@@ -42,7 +42,6 @@ func (handler HandlerFunc) RequestHandler(w http.ResponseWriter, r *http.Request
 
 	targetId := urlSplit[0]
 	targetRoute := "/" + urlSplit[1]
-	// log.Println("Splitted URL: ", urlSplit)
 
 	// Validate
 	if targetId == "" || strings.Contains(targetId, "/") {
@@ -50,14 +49,26 @@ func (handler HandlerFunc) RequestHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	log.Println("Target ID: ", targetId)
-	log.Println("Cache: ", handler.ServerList)
+	// log.Println("Target ID: ", targetId)
+	// log.Println("Cache: ", handler.ServerList)
 
-	// Get the target server from DB or cache
-	// TODO: Implement the caching layer for the target servers. For now, we will just query the database for each request.
-	targetServerTuple, err := handler.DB.GetTargetUrlByApiKey(handler.Ctx, targetId)
-	targetServer := targetServerTuple.TargetUrl.String
-	// log.Printf("Target server for ID '%s': %v\n", targetId, targetServerTuple)
+	// 2. Get the target server from DB or cache
+	var targetServer string
+
+	// Check if the target server URL is already in the cache (ServerList)
+	serverUrl, ok := handler.ServerList[targetId]
+	if !ok {
+		targetServerTuple, err := handler.DB.GetTargetUrlByApiKey(handler.Ctx, targetId)
+		if err != nil {
+			log.Printf("Error fetching target server for ID '%s': %v\n", targetId, err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+
+		targetServer = targetServerTuple.TargetUrl.String
+		handler.ServerList[targetId] = targetServer
+	} else {
+		targetServer = serverUrl
+	}
 
 	if targetServer == "" {
 		log.Printf("No target server found for ID '%s'\n", targetId)
@@ -65,12 +76,7 @@ func (handler HandlerFunc) RequestHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	// 2. Properly construct the URL, including query parameters
-	// The old url creation logic was removed. It was very inefficient
-	// creating a new URL for each request is unnecessary
-	// we can just concatenate the base server URL with the request path and query parameters.
-	//
-	// The old method used to use a base URL, with port number, and then add the URL path and query params to it.
+	// 3. Properly construct the URL, including query parameters
 	// Get the query parameters and append them to the target URL if they exist
 	var targetUrl string
 
@@ -87,7 +93,7 @@ func (handler HandlerFunc) RequestHandler(w http.ResponseWriter, r *http.Request
 		log.Println("Routing request to: ", targetUrl)
 	}
 
-	// 3. Simplify request creation
+	// 4. Simplify request creation
 	// Using NewRequestWithContext is best practice so the request cancels if the client disconnects early
 	proxyReq, err := http.NewRequestWithContext(r.Context(), r.Method, targetUrl, r.Body)
 	if err != nil {
@@ -96,23 +102,15 @@ func (handler HandlerFunc) RequestHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	// 4. Copy original request headers to the proxy request
+	// 5. Copy original request headers to the proxy request
 	// maps.Copy is a convenient way to copy all headers without needing to loop through them manually
 	// manually copying headers with .Header.Add does more things behind the scenes, like checking for correct header formatting
 	// which is unnecessary here since we're just copying them as-is,
 	// so using maps.Copy is more efficient and less error-prone.
 	maps.Copy(proxyReq.Header, r.Header)
 
-	// 5. Actually execute the request using an HTTP client
-	// NOTE: Each request consumes a token from the bucket of the allocated server.
-	// If the bucket is empty, it will block until a token is available.
-	// Stays blocked until a token is available.
-
-	// <-tokenBucket[targetServer]
-	// defer func() {
-	// 	// Return the token to the bucket after the request is done
-	// 	tokenBucket[targetServer] <- 1
-	// }()
+	// 6. Apply policies here (authentication, rate limiting, etc.)
+	// TODO: Fetch policies from DB and apply them to the request
 
 	resp, err := handler.Client.Do(proxyReq)
 	if err != nil {
@@ -122,42 +120,18 @@ func (handler HandlerFunc) RequestHandler(w http.ResponseWriter, r *http.Request
 	}
 	defer resp.Body.Close()
 
-	// 6. Copy backend response headers back to the client response
-	// Refer to point 4 for why maps.Copy is used here as well
+	// 7. Copy backend response headers back to the client response
+	// Refer to point 5 for why maps.Copy is used here as well
 	maps.Copy(w.Header(), resp.Header)
 
-	// 7. Write the exact status code returned by the backend
+	// 8. Write the exact status code returned by the backend
 	w.WriteHeader(resp.StatusCode)
 
-	// 8. Stream the body directly to avoid blowing up memory
+	// 9. Stream the body directly to avoid blowing up memory
 	// Copying the response body directly prevents loading the entire response into memory
 	// which is crucial for large responses
 	_, err = io.Copy(w, resp.Body)
 	if err != nil {
 		log.Printf("Error streaming response body: %v", err)
-	}
-}
-
-func (handler HandlerFunc) HandleRequest(w http.ResponseWriter, r *http.Request) {
-	// 1. Validate the request
-	fmt.Println("Received request:", r.Method, r.URL.Path)
-	fmt.Println("Headers:", r.Header)
-	fmt.Println("Body:", r.Body)
-
-	// 2. Apply policies (e.g., rate limiting, authentication)
-	// 3. Create a context for the request
-
-	// 4. Fire the request
-	// 5. After a response is generated, apply response policies (e.g., logging, modifying headers)
-	// 6. Send the response back to the client
-}
-
-func (handler HandlerFunc) IndexRouteHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-
-	case http.MethodGet:
-		w.Write([]byte("Welcome to Naamon!\n"))
-	case http.MethodPost:
-		w.Write([]byte("POST request received at /\n"))
 	}
 }
